a571cf5efc18838be7050ffce63912ec37933fc5
Laarni Astrid <laarni.astrid@gmail.com>
2016-02-26 11:17:46 -0700

    added whale template/content ctrl and content template/directive

--- directives/contentD.js
+++ directives/contentD.js
@@ -5,40 +5,11 @@ angular.module('myApp')
   return {
     retrict: 'E',
     templateUrl: '../views/content.html',
-    controller: '../js/content.js',
+    controller: 'whlCntCtrl',
     scope: {
-      
-    }
+      text: '=',
+    },
+
   }
 
 }) // end contentD directive
-
-
-angular.module('myApp')
-
-.directive('doStuff', function() {
-
-  return {
-    restrict: 'E',
-    templateUrl: '../views/doStuff.html',
-    scope: {
-      route: '=',
-      text: '=',
-      boxImg: '=',
-    },
-    link: function(scope, ele, attr) {
-      ele.on('mouseover', function() {
-        $('.content-box', this).css({
-          'border': '#2A9CE8 5px solid',
-        })
-      })
-      ele.on('mouseout', function() {
-        $('.content-box', this).css({
-          // 'background': '#006699',
-          'border': 'none',
-        })
-      })
-    },
-  }
-
-}) // end of footerD directive
---
+++ js/whlCntCtrl.js
@@ -0,0 +1,7 @@
+angular.module('myApp')
+
+.controller('whlCntCtrl', function($scope) {
+
+  $scope.whalesPageText = "this is a test";
+
+}) // end mainCtrl controller
