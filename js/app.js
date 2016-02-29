bd420a0bd581f80858780c57ea68c336a8ac0992
Laarni Astrid <laarni.astrid@gmail.com>
2016-02-29 16:35:41 -0700

    added home.css styles to main.css and removed home.css file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('myApp', ['ui.router', 'ngAnimate'])
 
 .config(function($stateProvider, $urlRouterProvider) {
-;
+
   $stateProvider
     .state('home', {
       url: '/',
--- js/contactCtrl.js
+++ js/contactCtrl.js
@@ -4,7 +4,10 @@ angular.module('myApp')
 
   $scope.contactPageText = 'this is the contact page';
 
-
+  $scope.addPost = function() {
+    console.log($scope.contact);
+    $scope.contact = '';
+  }
 
 
 }) // end contactCtrl controller
