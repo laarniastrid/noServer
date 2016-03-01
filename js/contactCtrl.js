4ee81772610a9ea27979721a51b4eb6ef2e878eb
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-01 11:11:25 -0700

    add gif to top

--- js/contactCtrl.js
+++ js/contactCtrl.js
@@ -4,20 +4,20 @@ angular.module('myApp')
 
   $scope.contactPageText = 'this is the contact page';
 
-  $scope.addPost = function(data) {
+  $scope.addPost = function() {
 
-    if (!data) {
-      console.error('fill in the form');
-    }
-
-    data = JSON.stringify(data, undefined, 2);
-
-    var stuff = new Blob([data], {type: 'text/json'});
-    var send = document.getElementById('contact-submit');
-
-    send.download = new Date();
-    send.href = window.URL.createObjectURL(stuff);
-    send.dataset.downloadurl = ['text/json', send.download, send.href].join(':');
+    // if (!data) {
+    //   console.error('fill in the form');
+    // }
+    //
+    // data = JSON.stringify(data, undefined, 2);
+    //
+    // var stuff = new Blob([data], {type: 'text/json'});
+    // var send = document.getElementById('contact-submit');
+    //
+    // send.download = new Date();
+    // send.href = window.URL.createObjectURL(stuff);
+    // send.dataset.downloadurl = ['text/json', send.download, send.href].join(':');
 
 
     console.log($scope.contact);
