a28c6b7e5555b0dcf05178e7aef2019f42c36900
Laarni Astrid <laarni.astrid@gmail.com>
2016-02-29 17:26:04 -0700

    did stuff

--- js/animations.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  console.log('hello there');
-
-
-
-
-}) // end document ready
--- js/contactCtrl.js
+++ js/contactCtrl.js
@@ -4,7 +4,22 @@ angular.module('myApp')
 
   $scope.contactPageText = 'this is the contact page';
 
-  $scope.addPost = function() {
+  $scope.addPost = function(data) {
+
+    if (!data) {
+      console.error('fill in the form');
+    }
+
+    data = JSON.stringify(data, undefined, 2);
+
+    var stuff = new Blob([data], {type: 'text/json'});
+    var send = document.getElementById('contact-submit');
+
+    send.download = new Date();
+    send.href = window.URL.createObjectURL(stuff);
+    send.dataset.downloadurl = ['text/json', send.download, send.href].join(':');
+
+
     console.log($scope.contact);
     $scope.contact = '';
   }
