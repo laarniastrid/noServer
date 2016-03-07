886a780c36348b0bcbdb905436451e411d453e2c
Laarni Astrid <laarni.astrid@gmail.com>
2016-03-06 19:41:11 -0700

    did stuff

--- js/app.js
+++ js/app.js
@@ -45,5 +45,5 @@ angular.module('myApp', ['ui.router', 'ngAnimate'])
     }) // end contact
 
 
-    // $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 }) // end of app config
